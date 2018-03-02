32d2bfd3c7f3c8bb202b78715c8072c3f4b9da54
wlous <louiseahokas@gmail.com>
2018-03-02 11:50:16 +0100

    Changed App.java to ProductServiceApp

---
+++ src/main/java/com/product/ProductServiceApp.java
@@ -0,0 +1,41 @@
+package com.product;
+
+import java.util.Date;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Link;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * JAX-RS application. Root path is /trivia
+ *
+ */
+@ApplicationPath("/webshop")
+@Path("")
+
+public class ProductServiceApp extends Application
+{
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getDirectory(@Context UriInfo uri) {
+        Link selfLink = Link.fromUri(uri.getBaseUri())
+                .rel("self").type(MediaType.APPLICATION_JSON)
+                .build();
+        Link productsLink = Link.fromUri(uri.getBaseUri() + "products")
+                .rel("products").type(MediaType.APPLICATION_JSON)
+                .build();
+
+        return Response.ok()
+                .lastModified(new Date())
+                .location(uri.getRequestUri())
+                .links(selfLink, productsLink)
+                .build();
+    }
+    
+}
